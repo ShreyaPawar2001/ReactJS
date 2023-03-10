691e5079e589022531d858771572ae4e1f2fd340
ShreyaPawar2001 <shreyapawar2001@gmail.com>
2023-03-10 15:47:03 +0530

    LetsStartCodingItems

---
+++ src/components/Expenseltem.js
@@ -0,0 +1,15 @@
+function Expenseltem() {
+    return (
+      <div>
+        <div><h2>Expenseltem</h2></div>
+        <div>Food Rs 10</div>
+        <div>Petrol Rs 100</div>
+        <div>Movies Rs 200</div>
+        <div>
+          <h2>Done</h2>
+        </div>
+        
+      </div>
+    );
+  }
+  export default Expenseltem;
