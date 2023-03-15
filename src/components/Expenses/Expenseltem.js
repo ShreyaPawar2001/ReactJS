a450d25c5c2ef6b7a0e67bdf6a3fb3681681dd76
ShreyaPawar2001 <shreyapawar2001@gmail.com>
2023-03-15 11:41:49 +0530

    AddedEventListener

--- src/components/Expenses/Expenseltem.js
+++ src/components/Expenses/Expenseltem.js
@@ -5,6 +5,10 @@ import './Expenseltem.css';
 
 
 const Expenseltem = (props) => { // props are used for passinf data from a to b file.
+  const clickHandler = () =>{
+    console.log("clicked!!");
+  };
+  
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
@@ -13,7 +17,10 @@ const Expenseltem = (props) => { // props are used for passinf data from a to b 
         <h2 >{props.title}</h2>
         <div className='expense-item__description'>{props.location}</div>
         <div className='expense-item__price'>${props.amount}</div>
+        <button onClick={clickHandler}>Change title</button>
+        <button >Delete</button>
       </div>
+      
     </Card>
   );
 }
